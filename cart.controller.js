f9a00128a994aa811133ed5f669a0bab9e39cc6a
K1REBEL <kibraheem47@yahoo.com>
2024-01-18 17:25:49 +0200

    some work done

---
+++ cart.controller.js
@@ -0,0 +1,102 @@
+// import {Usr} from "./fetchApi.js"
+import {Usr} from "./fetchApi.js"
+
+console.log(Usr);
+
+let users = JSON.parse(localStorage.getItem("users")) || []
+
+function fetchUser(user){
+   for (let i = 0; i < users.length; i++) {
+      if (user.username == users[i].username && users[i].logged_in == true){
+         // console.log("I'm here");
+         // console.log(users[i]);
+         return users[i]
+      }
+   }
+   console.log("not found");
+}
+
+var Usr1 = fetchUser(Usr)
+
+var cart = Usr1.cart;
+
+
+window.onload = function(cart) {
+   // var cart = [];
+   console.log(cart);
+   var cartItemsElement = document.querySelector(".product");
+   // var totalCostElement = document.getElementById('total-cost');
+   console.log(cartItemsElement);
+   cartItemsElement.innerHTML = '';
+   
+   for(let i=0; i<cart.length; i++){
+      console.log("hi");
+      var cartItem = document.createElement('li');
+      cartItem.textContent = `${cart[i].name} - Quantity: ${cart[i].quantity} - $${(cart[i].price * cart[i].quantity).toFixed(2)}`;
+      cartItemsElement.appendChild(cartItem);
+      console.log(cartItemsElement);
+   }
+}
+
+
+// document.addEventListener("DOMContentLoaded",function(cart){
+//    console.log(cart);
+//    var cartItemsElement = document.querySelector(".product");
+//    // var totalCostElement = document.getElementById('total-cost');
+//    console.log(cartItemsElement);
+//    cartItemsElement.innerHTML = '';
+   
+//    for(let i=0; i<cart.length; i++){
+//       console.log("hi");
+//       var cartItem = document.createElement('li');
+//       cartItem.textContent = `${cart[i].name} - Quantity: ${cart[i].quantity} - $${(cart[i].price * cart[i].quantity).toFixed(2)}`;
+//       cartItemsElement.appendChild(cartItem);
+//       console.log(cartItemsElement);
+//    }
+ 
+//    // cart.forEach(itemm => {
+//    //     var cartItem = document.createElement('li');
+//    //     cartItem.textContent = `${itemm.name} - Quantity: ${itemm.quantity} - $${(itemm.price * itemm.quantity).toFixed(2)}`;
+//    //     cartItemsElement.appendChild(cartItem);
+//    // });
+// }) 
+
+// document.addEventListener("DOMContentLoaded",function(){
+//    let products = document.querySelector(".product");
+//    var json;
+//    fetch('https://dummyjson.com/products')
+//    .then(res=>res.json())
+//    .then(fetchedJson=> {
+//      json = fetchedJson;
+//     //  console.log(json.products);
+//      json.products.forEach(data => {
+//      products.innerHTML += `
+//                  <div class="product filter-item all mobiles">
+//                     <div class="box">
+//                       <div class="image">
+//                         <img src="${data.thumbnail}" alt="">
+//                       </div>
+//                        <div class="text">
+//                        <p class="category"> ${data.category}</p>
+//                      <p class="product-name">${data.title}</p>
+//                      <p class="price">${data.price}</p>
+//                      <p class="description">${data.description}</p>
+//               <button class="purchase" onclick="addToCart(${data.id})"><a target="_blank" href="./cart.html">Add to Cart</a></button>
+//                  </div>
+//              </div>
+//              </div>`
+//    })
+//  })
+//  });
+
+// async function updateCart() {
+//   try {
+   
+//    //   var cart3 = await addToCart(1, cart1);
+//      console.log("Current User Cart: ", cart1);
+//   } catch (error) {
+//      console.error("Error updating cart:", error);
+//   }
+// }
+// updateCart();
+
--- fetchApi.js
+++ fetchApi.js
@@ -1,3 +1,7 @@
+import {newUser} from "./LoginSignup.js"
+// console.log(newUser);
+
+
 document.addEventListener("DOMContentLoaded",function(){
   let products = document.querySelector(".product");
   var json;
@@ -5,7 +9,7 @@ document.addEventListener("DOMContentLoaded",function(){
   .then(res=>res.json())
   .then(fetchedJson=> {
     json = fetchedJson;
-    console.log(json.products);
+   //  console.log(json.products);
     json.products.forEach(data => {
     products.innerHTML += `
                 <div class="product filter-item all mobiles">
@@ -26,40 +30,63 @@ document.addEventListener("DOMContentLoaded",function(){
 })
 });
 
-function addToCart(id, cart) {
-  return new Promise((resolve, reject) => {
-     fetch('https://dummyjson.com/products/' + id)
-        .then(res => res.json())
-        .then(product => {
-           var item = cart.find(p => p.id === product.id);
-           if (item) {
-              item.quantity++;
-           } else {
-              cart.push({ id: product.id, name: product.title, price: product.price, quantity: 1 });
-           }
-           console.log("cart: ", cart);
-           resolve(cart); 
-        })
-        .catch(error => {
-           console.error("Error fetching product:", error);
-           reject(error); 
-        });
-  });
+let users = JSON.parse(localStorage.getItem("users")) || []
+
+function fetchUser(user){
+   for (let i = 0; i < users.length; i++) {
+      if (user.username == users[i].username && users[i].logged_in == true){
+         // console.log("I'm here");
+         // console.log(users[i]);
+         return users[i]
+      }
+   }
+   console.log("not found");
 }
 
+var Usr = fetchUser(newUser)
 
-var as = addToCart(1);
+function addToCart(id, Usr) {
+   var cart = Usr.cart;
+   fetch('https://dummyjson.com/products/' + id)
+     .then(res => res.json())
+     .then(product => {
+       var item = cart.find(p => p.id === product.id);
+       if (item) {
+         item.quantity++;
+       } else {
+         cart.push({ id: product.id, name: product.title, price: product.price, quantity: 1 });
+       }
+       Usr.cart = cart
+       for (let i = 0; i < users.length; i++) {
+         if (Usr.username == users[i].username && users[i].logged_in == true){
+            // console.log("I'm here");
+            users[i] = Usr
+            // console.log(users[i]);
+            localStorage.setItem("users", JSON.stringify(users))
+            break
+         }
+      }
+      //  console.log(cart);
+       return Usr;
+     });
+ }
+
+ var as = addToCart(1, Usr);
+
+// console.log(Usr);
+
+export {Usr}
 
 
-var cart1 = currentUser.cart;
+// var cart1 = currentUser.cart;
 
-async function updateCart() {
-  try {
-     var cart3 = await addToCart(1, cart1);
-     console.log("cart3: ", cart3);
-  } catch (error) {
-     console.error("Error updating cart:", error);
-  }
-}
+// async function updateCart() {
+//   try {
+//      var cart3 = await addToCart(1, cart1);
+//      console.log("cart3: ", cart3);
+//   } catch (error) {
+//      console.error("Error updating cart:", error);
+//   }
+// }
 
-updateCart();
+// updateCart();
