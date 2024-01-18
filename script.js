8c24daa136eadc212da87f53245c0db4e3b42944
menna ashraf <you@example.com>
2024-01-18 22:11:36 +0200

    Home page update

--- script.js
+++ script.js
@@ -7,6 +7,37 @@ window.addEventListener('scroll' , function()
 
 /////////////////////////////////////
 
+document.addEventListener("DOMContentLoaded", function () 
+{
+  const navToggle = document.querySelector(".nav-toggle")
+  const navigation = document.querySelector(".navigation")
+
+  navToggle.addEventListener("click", function () 
+  {
+    document.body.classList.toggle("nav-open")
+    navigation.classList.toggle("show")
+  })
+
+  const navigationLinks = document.querySelectorAll(".navigation a")
+  navigationLinks.forEach(function (link) 
+  {
+    link.addEventListener("click", function () 
+    {
+      document.body.classList.remove("nav-open")
+      navigation.classList.remove("show")
+    })
+  })
+
+  window.addEventListener("scroll", function () 
+  {
+    const header = document.querySelector("header")
+    header.classList.toggle("down", window.scrollY > 0)
+  })
+})
+
+
+////////////////////////////////////
+
 $(document).ready(function () 
 {
   
