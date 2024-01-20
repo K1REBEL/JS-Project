02842a0bb5da569cef10f670364ea104b28f8161
menna ashraf <you@example.com>
2024-01-20 20:46:11 +0200

    logout

--- logout.js
+++ logout.js
@@ -1,31 +1,31 @@
-
-let users = JSON.parse(localStorage.getItem("users")) || []
-let currentUser = JSON.parse(localStorage.getItem("Current User"))
-console.log(currentUser)
+let users = JSON.parse(localStorage.getItem("users")) || [];
+let currentUser = JSON.parse(localStorage.getItem("Current User"));
+// console.log(currentUser)
 
 function logout(user) {
-   if(user.logged_in){
-      user.logged_in = false
-      for (let i = 0; i < users.length; i++) {
-         if (user.username == users[i].username && users[i].logged_in == true) {
-           users[i] = user
-           localStorage.setItem("users", JSON.stringify(users))
-           localStorage.setItem("Current User", JSON.stringify(user))
-           break
-         }
-       }
-      alert(`User ${user.username} logged out due to inactivity.`)
-   }
-   else{ alert("User is already logged out") }
+  if (user.logged_in) {
+    user.logged_in = false;
+    for (let i = 0; i < users.length; i++) {
+      if (user.username == users[i].username && users[i].logged_in == true) {
+        users[i] = user;
+        localStorage.setItem("users", JSON.stringify(users));
+        localStorage.setItem("Current User", JSON.stringify(user));
+        break;
+      }
+    }
+    alert(`User ${user.username} logged out due to inactivity.`);
+  } else {
+    alert("User is already logged out");
+  }
 }
 
-setTimeout(() => logout(currentUser), 300000)
+setTimeout(() => logout(currentUser), 300000);
 
 // function logoutBTN(){
 //    // console.log("Hello from button invoked Fn.", newUser)
 //    if(newUser.logged_in){
 //       newUser.logged_in = false
-//       console.log(`User ${newUser.username} logged out due to inactivity.`)
+//       console.log(User ${newUser.username} logged out due to inactivity.)
 //    }
 //    else{ console.log("User is already logged out") }
 // }
@@ -34,10 +34,9 @@ setTimeout(() => logout(currentUser), 300000)
 // logoutBtn.addEventListener('click', logoutBTN)
 
 function resetLogoutTimer() {
-//   clearTimeout(logout);
-//   logout(currentUser);
+  //   clearTimeout(logout);
+  //   logout(currentUser);
 }
 
 document.addEventListener("mousemove", resetLogoutTimer);
 document.addEventListener("keypress", resetLogoutTimer);
-
